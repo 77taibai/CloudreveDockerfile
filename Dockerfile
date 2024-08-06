67c327c90f10100bc03fc8b0ca12d5c6d192927e
77taibai <86600901+77taibai@users.noreply.github.com>
2024-08-06 18:18:02 +0800

    大修改

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,13 @@ COPY . .
 
 RUN apt update \
     
-    && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata pgbouncer libpq-dev \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata pgbouncer dos2unix libpq-dev \
 
     && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
 
     && tar -zxvf ./cloudreve_3.8.3_linux_amd64.tar.gz \
+
+    && dos2unix start.sh \
 
     && chmod +x ./start.sh
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uname -a
+chmod -R 777 /cloudrve
 
 cat > /cloudreve/pgbouncer.ini <<-EOF
 [databases]
@@ -19,10 +19,13 @@ max_client_conn = 80
 default_pool_size = 20
 reserve_pool_size = 5
 dns_max_ttl = 15
+ignore_startup_parameters = extra_float_digits 
+client_tls_sslmode=disable
+client_tls_sslmode=require
 EOF
 
 cat > /cloudreve/userlist.txt <<-EOF
-"$DB_USER" "$DB_PASSWORD"
+"$DB_USER" "endpoint=$DB_ENDPOINT\$$DB_PASSWORD"
 EOF
 
 echo "PgBouncer配置文件创建完毕"
@@ -33,28 +36,22 @@ chmod 777 /cloudreve/pgbouncer.log
 
 echo "PgBouncer依赖文件创建完毕"
 
-pgbouncer --version
-
-#pgbouncer -d -u nobody /cloudreve/pgbouncer.ini
-
-#pgbouncer -R -d -u nobody /cloudreve/pgbouncer.ini
+pgbouncer -d -u nobody /cloudreve/pgbouncer.ini
 
 echo "PgBouncer开始运行"
-
-pgbouncer -R -u nobody /cloudreve/pgbouncer.ini
 
 cat > /cloudreve/conf.ini <<-EOF
 [System]
 Debug = false
 Mode = master
 Listen = :5212
-SessionSecret = DR3c3K0P6ei6rLJDm8ffNdV4uLs2jAXnOvWDmmDPW7C5KhYtuRijM6N6x8KJMqcc
-HashIDSalt = 4brAt3NOAOmNo79S0OoU3BADYVRSrrEuRbrudTSLPDHNQ25C2UN3cwsPfJMA0wUr
+SessionSecret = $SESSION_SECRET
+HashIDSalt = $HASH_ID_SALT
 [Database]
 Type = postgres
 Port = 37721
 User = $DB_USER
-Password = $DB_PASSWORD
+Password = endpoint=$DB_ENDPOINT\$$DB_PASSWORD
 Host = 127.0.0.1
 Name = $DB_NAME
 TablePrefix = cd
@@ -65,4 +62,4 @@ chmod +x /cloudreve/cloudreve
 
 echo "准备运行Cloudreve"
 
-#./cloudreve
+./cloudreve
